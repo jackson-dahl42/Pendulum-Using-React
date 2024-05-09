1cb9fcbcf7268547e0bac5025ed6b839e1007b75
Jackson Dahl <99228002+jackson-dahl42@users.noreply.github.com>
2024-05-09 09:40:53 +0000

    Added canvas and animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import './App.css'
+import Canvas from './Canvas';
 
-export default function App() {
+let velocity = 0
+let angle = 10
+let gravity = 9.8
+let damping = 0.1
+let length = 10
+
+function App() {
+
+  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+        let acceleration = (-gravity / length) * Math.sin(angle) - (damping * velocity)
+        velocity += acceleration
+        angle += velocity
+        const pivotx = 100;
+        const pivoty = 100;
+        const bobX = pivotx + length * Math.sin(angle);
+        const bobY = pivoty + length * Math.cos(angle);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        ctx.beginPath();
+        ctx.arc(bobX, bobY, 5, 0, Math.PI * 2);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(pivotx, pivoty);
+        ctx.lineTo(bobX, bobY);
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+  };
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    
+        <Canvas draw={draw} />
+  );
 }
+
+export default App;
